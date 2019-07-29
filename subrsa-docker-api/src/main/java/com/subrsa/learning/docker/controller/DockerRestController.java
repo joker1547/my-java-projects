45b4f201cd1bf7848435a13da507f260cff5b17c
Santhose Subramaniam <santhose.subramaniam@gmail.com>
2019-07-28 22:00:56 -0400

    organize imports

--- subrsa-docker-api/src/main/java/com/subrsa/learning/docker/controller/DockerRestController.java
+++ subrsa-docker-api/src/main/java/com/subrsa/learning/docker/controller/DockerRestController.java
@@ -4,9 +4,7 @@ import com.github.dockerjava.api.model.Image;
 import com.subrsa.learning.docker.service.DockerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +15,7 @@ public class DockerRestController {
     @Autowired
     DockerService dockerService;
 
-    @GetMapping("/images")
+    @GetMapping("/api/docker/images")
     public List<Image> getImages() {
         try {
             List<Image> images = dockerService.getAvailableImages();
